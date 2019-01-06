3c13dad9f8d653f1650679977971f21244c8458c
artiveloper <artiveloper@gmail.com>
2019-01-06 23:59:38 +0900

    Add ArrayList

---
+++ src/main/java/me/artiveloper/learning/ArrayList.java
@@ -0,0 +1,54 @@
+package me.artiveloper.learning;
+
+public class ArrayList {
+
+    private Object[] data;
+    private int size;
+    private int index;
+
+    public ArrayList() {
+        this.size = 1;
+        this.data = new Object[this.size];
+        this.index = 0;
+    }
+
+    public void add(Object obj) {
+        if (this.index == this.size - 1) {
+            doubling();
+        }
+        this.data[this.index] = obj;
+        this.index++;
+    }
+
+    private void doubling() {
+        this.size = this.size * 2;
+        Object[] newData = new Object[this.size];
+        for (int i = 0; i < this.data.length; i++) {
+            newData[i] = this.data[i];
+        }
+        this.data = newData;
+    }
+
+    public Object get(int i) throws Exception {
+        if (i > this.index - 1) {
+            throw new Exception("ArrayIndexOutOfBound");
+        } else if (i < 0) {
+            throw new Exception("Negative Value");
+        }
+        return this.data[i];
+    }
+
+    public void remote(int i) throws Exception {
+        if (i > this.index - 1) {
+            throw new Exception("ArrayIndexOutOfBound");
+        } else if (i < 0) {
+            throw new Exception("Negative Value");
+        }
+        for (int x = i; x < this.data.length - 1; x++) {
+            data[x] = data[x + 1];
+        }
+        this.index--;
+
+    }
+
+}
---
+++ src/test/java/me/artiveloper/learning/ArrayListTest.java
@@ -0,0 +1,28 @@
+package me.artiveloper.learning;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class ArrayListTest {
+
+    @Test
+    public void test() throws Exception {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("0");
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayList.add("4");
+        arrayList.add("5");
+        arrayList.add("6");
+        arrayList.add("7");
+        arrayList.add("8");
+        arrayList.add("9");
+
+        assertThat(arrayList.get(5)).isEqualTo("5");
+        arrayList.remote(5);
+        assertThat(arrayList.get(5)).isEqualTo("6");
+    }
+
+}
